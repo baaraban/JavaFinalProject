da1cdc4cab2114bac357923dbbb021f5ec6522e0
perepichka <perepichka@ucu.edu.ua>
2017-12-25 21:43:46 +0200

    Add tests

---
+++ src/test/java/test/FootballRepositoryTest.java
@@ -0,0 +1,16 @@
+package test;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FootballRepositoryTest {
+    @Test
+    public void doWork() throws Exception {
+    }
+
+    @Test
+    public void workWithDataFrame() throws Exception {
+    }
+
+}
